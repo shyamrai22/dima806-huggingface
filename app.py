bde9ef9d2ab7ed2d7bf1e588c24422677b7d74b2
Shyam Rai <shyamrrai0011@gmail.com>
2024-11-16 04:26:37 +0530

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from transformers import pipeline
 from PIL import Image
-import io
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+# Load the model
 emotion_model = pipeline("image-classification", model="dima806/facial_emotions_image_detection")
 
 @app.route('/detect_emotion', methods=['POST'])
@@ -14,15 +17,16 @@ def detect_emotion():
 
         # Load and preprocess the image
         image_file = request.files['image']
-        image = Image.open(image_file).convert("RGB")  # Ensure image is in RGB format
+        image = Image.open(image_file).convert("RGB")
 
         # Run the model
         predictions = emotion_model(image)
-        
+
         # Return the top predictions
         return jsonify(predictions), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
